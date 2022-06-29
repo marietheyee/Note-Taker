e05c88b670bed13e5fe8e7d2b5b97b6f5e8d00cf
Marie Yee <marieyee02@yahoo.com>
2022-06-29 16:23:06 -0700

    updated server.js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
-//npm package for unique ids
 var uniqid = require('uniqid');
+
+
 
 module.exports = (app) => {
 
-    //GET /api/notes to read json file
     app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, "../db/db.json"));
+        res.sendFile(path.join (__dirname, '../db/db.json'));
     });
 
-    //POST /api/notes add new note to db.json file
-    app.post('/api/notes', (req, res) => {
+    app.post('/api/notes,', req, res) => {
         let db = fs.readFileSync('db/db.json');
         db = JSON.parse(db);
         res.json(db);
@@ -22,19 +21,11 @@ module.exports = (app) => {
             text: req.body.text,
             id: uniqid(),
         };
-        //push created note to write in db.json file
+
         db.push(userNote);
         fs.writeFileSync('db/db.json', JSON.stringify(db));
         res.json(db);
-    });
 
-    //DELETE will receive a query parameter with id of a note to delete
-    app.delete('/api/notes/:id', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'))
-        
-        let deleteNotes = db.filter(item => item.id !== req.params.id);
 
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-        res.json(deleteNotes);
-    }) 
-};
+    }
+}
