2e64becb5d9fceb9c32c78201d0b86576d1908b3
Marie Yee <marieyee02@yahoo.com>
2022-06-29 13:57:29 -0700

    commented server.js file

--- server.js
+++ server.js
@@ -1,58 +1,19 @@
-//create const "requires"
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const notes = require('notes');
-
-
-//middleware
+//use express
 const app = express();
-const PORT = process.env.PORT || 3001;
+//set up PORT
+const PORT = process.env.PORT || 3001
 
-//data parsing
+//asking express to create route for files under 'public'
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
-//function to read data and append some content
-const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-        }
-    });
-};
+//api route and html route
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app)
 
-//GET route for retrieving Notes
-notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then ((data) => res.json(JSON.parse(data)));
-});
-
-//POST Route for a new UX/UI note
-notes.post('/', (req, res) => {
-    console.log(req.body);
-    
-    const { note_title, text } = req.body;
-    if (req.body) {
-        const newNote = {
-            note_title,
-            text,
-        };
-
-        readAndAppend(newNote, './db/db/json');
-        res.json(`Note added successfully`);
-    } else {
-        res.error('Error in adding note');
-    }
+//app listener
+app.listen(PORT, () => {
+    console.log(`Server available at localhost${PORT}`);
 })
-
-
-//listener
-app.listen(PORT, () => 
-console.log(`App listening at http://localhost:${PORT}`)
-);
